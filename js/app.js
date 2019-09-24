c9bb1661b30d688ee7428f816dadd2eea80fb1f2
Anton Liubimov <ant.liubimov@gmail.com>
2019-09-24 16:45:04 +0500

    Add function operation

--- js/app.js
+++ js/app.js
@@ -1,63 +1,92 @@
-const screenInput = document.querySelector('.screen-input');
-const screenOutput = document.querySelector('.screen-output');
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach(btn => btn.addEventListener('click', btnClick));
-
 let calc = {
   operation: null,
-}
+  result: null,
+  num1: null,
+  num2: null,
+  arr: [],
+};
 
-let num1 = null;
-let num2 = null;
-let result = null;
+const screenInput = document.querySelector(".screen-input");
+const screenOutput = document.querySelector(".screen-output");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(btn => btn.addEventListener("click", btnClick));
 
 function btnClick(e) {
-  if (e.target.hasAttribute('data-number')) {
-    writeNumber.call(this, e.target.getAttribute('data-number'));
-  } else if (e.target.hasAttribute('data-key')) {
-    operateMath.call(this, e.target.getAttribute(('data-key')));
+  if (e.target.hasAttribute("data-number")) {
+    writeNumber.call(this, e.target.getAttribute("data-number"));
+  } else if (e.target.hasAttribute("data-key")) {
+    operateMath.call(this, e.target.getAttribute("data-key"));
   }
 }
 
 function writeNumber(num) {
   if (screenOutput.innerText.length < 13) {
-    (screenOutput.innerText === '0') ? screenOutput.innerText = num : screenOutput.innerText += num;
+    if (calc.result !== null) {
+      calc.num1 = calc.result;
+      clearOutput();
+    }
+    screenOutput.innerText === "0"
+      ? (screenOutput.innerText = num)
+      : (screenOutput.innerText += num);
     screenInput.innerText += num;
   }
 }
 
 function operateMath(key) {
-  console.log(key);
   switch (key) {
-    case 'C':
-      screenOutput.innerText = '0';
-      screenInput.innerText = '';
-      num1 = null;
-      num2 = null;
-      result = null;
+    case "C":
+      screenOutput.innerText = "0";
+      screenInput.innerText = "";
+      calc.operation = null;
+      calc.num1 = null;
+      calc.num2 = null;
+      calc.result = null;
+      calc.arr = [];
       break;
-    case '+/-':
-      screenOutput.innerText = screenOutput.innerText * (-1);
+    case "+/-":
+      screenOutput.innerText = screenOutput.innerText * -1;
       break;
-    case 'del':
+    case "del":
       break;
-    case '.':
-      if (!screenOutput.innerText.includes('.')) {
-        screenOutput.innerText += '.';
+    case ".":
+      if (!screenOutput.innerText.includes(".")) {
+        screenOutput.innerText += ".";
       }
       break;
-    case '%':
+    case "%":
+
       break;
-    case '/':
+    case "/":
+      checkOperation('/', divide);
+      checkNums();
+      calc.arr.push('/');
+      calc.operation = divide;
+      screenInputText();
       break;
-    case '*':
+    case "*":
+      checkOperation('*', multiply);
+      checkNums();
+      calc.arr.push('*');
+      calc.operation = multiply;
+      screenInputText();
       break;
-    case '-':
+    case "-":
+      checkOperation('-', subtract);
+      checkNums();
+      calc.arr.push('-');
+      calc.operation = subtract;
+      screenInputText();
       break;
-    case '+':
+    case "+":
+      checkOperation('+', add);
+      checkNums();
+      calc.arr.push('+');
+      calc.operation = add;
+      screenInputText();
       break;
-    case '=':
+    case "=":
+      calc.operation = '=';
       break;
   }
 }
@@ -75,9 +104,44 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+  if (b === 0) return 'Cannot divide by zero';
   return a / b;
 }
 
 function operate(operation, num1, num2) {
-  operation(num1, num2);
+  return operation(num1, num2);
+}
+
+function clearOutput() {
+  screenOutput.innerText = '0';
+}
+
+function checkOperation(oper, operation) {
+  if (calc.arr.length > 1 && (/[*\/+-]/g).test(calc.arr[calc.arr.length - 1])) {
+      calc.arr.pop();
+    calc.arr.push(oper);
+    calc.operation = operation;
+  }
+
+}
+
+function checkNums() {
+  if (calc.num1 === null) {
+    calc.num1 = Number(screenOutput.innerText);
+    calc.arr.push(calc.num1);
+    clearOutput();
+  }
+  if (calc.num1 !== null && calc.operation !== null) {
+    calc.num2 = Number(screenOutput.innerText);
+    calc.arr.push(calc.num2);
+    calc.result = operate(calc.operation, calc.num1, calc.num2);
+    calc.num1 = null;
+    calc.num2 = null;
+    calc.operation = null;
+    screenOutput.innerText = String(calc.result);
+  }
+}
+
+function screenInputText() {
+  screenInput.innerText = calc.arr.join('');
 }
